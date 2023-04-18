093367e9e4f57de941354eea0badf3ce327859c8
Anna Jungmannova <anna.jungmannova@accenture.com>
2023-04-18 17:07:24 +0200

    psi

--- School/2nd semestr/PSI/semestralka/server.py
+++ School/2nd semestr/PSI/semestralka/server.py
@@ -18,11 +18,17 @@ SERVER_LOGIN_ERROR = b"302 LOGIC ERROR\a\b"
 SERVER_KEY_OUT_OF_RANGE_ERROR = b"303 KEY OUT OF RANGE\a\b"
 # END
 
-def compareId(conn, send, want):
-    conn.send(b"" + send.encode() + b"\a\b")
-    clientKeyId = conn.recv(1024)[:-2].decode()
-    if clientKeyId == want:
-        print("[SECRETS MATCHED]")
+serverKeys = [23019,32037,18789,16443,18189]
+clientKeys = [32037,29295,13603,29533,21952]
+
+def compareId(conn, serverConfirmHash, clientConfirmHash):
+    conn.send(b"" + serverConfirmHash + b"\a\b")
+    print(f"[SERVER_CONFIRMATION w/ key {serverConfirmHash}]")
+    clientKeyId = conn.recv(1024)[:-2].decode()    
+    print("[CLIENT_CONFIRMATION got: {clientKeyId} want: {clientConfirmHash}]")
+    if clientKeyId == clientConfirmHash:
+        print("[SECRETS MATCHED; SERVER_OK]")
+        conn.send(SERVER_OK)
     elif any(char.isalpha() for char in clientKeyId):
         conn.send(SERVER_SYNTAX_ERROR)
         conn.close()
@@ -30,32 +36,74 @@ def compareId(conn, send, want):
         conn.send(SERVER_LOGIN_FAILED)
         conn.close()
 
-def auth(conn):
+def auth(conn, usernameHash):
     conn.send(SERVER_KEY_REQUEST)
     keyId = int(conn.recv(1024)[:-2].decode())
     print(f"[KEY ID IS: {keyId}]")
     # parse keyId to get only
-    if keyId == 0:
-        compareId(conn, "23019", "32037")
+    if keyId >= 0 and keyId <= 4:
+        serverConfirmHash = (usernameHash + serverKeys[keyId]) % 65536
+        clientConfirmHash = (usernameHash + clientConfirmHash[keyId]) % 65536
+        compareId(conn, serverConfirmHash, clientConfirmHash)
         print("send-delivered-checked")
-    elif keyId == 1:
-        compareId(conn, "32037", "29295")
-    elif keyId == 2:
-        compareId(conn, "18789", "13603")
-    elif keyId == 3:
-        compareId(conn, "16443", "29533")
-    elif keyId == 4:
-        compareId(conn, "18189", "21952")
+    elif keyId > 4 or keyId < 0:
+        print(f"[KEY ID, that should not be 0, 1, 2, 3 or 4, IS: {keyId}]")
+        conn.send(SERVER_KEY_OUT_OF_RANGE_ERROR)
+        conn.close()
     else:
-        if keyId > 4 or keyId < 0:
-            print(f"[KEY ID, that should not be 0, 1, 2, 3 or 4, IS: {keyId}]")
-            conn.send(SERVER_KEY_OUT_OF_RANGE_ERROR)
-            conn.close()
-        else:
-            print(f"[KEY ID, should be non-number, IS: {keyId}]")
-            conn.send(SERVER_LOGIN_ERROR)
-            conn.close()
+        print(f"[KEY ID, should be non-number, IS: {keyId}]")
+        conn.send(SERVER_LOGIN_ERROR)
+        conn.close()
 
+def handleObstacle(conn, currentCoords):
+    if currentCoords[0] > 0 and currentCoords[1] > 0:
+        conn.send(SERVER_TURN_LEFT)
+        conn.send(SERVER_MOVE)
+
+def getToCenter(conn):
+    coordsVisited = {}
+    conn.send(SERVER_TURN_LEFT)
+    resp = conn.recv(1024)[:-2].decode()
+    coordsVisited[0] = resp.split[2:](" ")
+    print(f"[FIRST COORDINATES: {coordsVisited[0][0]}:{coordsVisited[0][1]}]")
+    conn.send(SERVER_MOVE)
+    resp = conn.recv(1024)[:-2].decode()
+    coordsVisited[1] = resp.split[2:](" ")
+    print(f"[SECOND COORDINATES: {coordsVisited[1][0]}:{coordsVisited[1][1]}]")
+    # turn to center
+    if coordsVisited[0][0] < coordsVisited[0][1] and coordsVisited[0][1] > 0:
+        conn.send(SERVER_TURN_LEFT)
+        conn.send(SERVER_TURN_LEFT)
+    elif coordsVisited[0][0] > coordsVisited[0][1] and coordsVisited[0][0] < 0:
+        conn.send(SERVER_TURN_LEFT)
+        conn.send(SERVER_TURN_LEFT)
+    elif coordsVisited[1][0] < coordsVisited[1][1] and coordsVisited[1][1] > 0:
+        conn.send(SERVER_TURN_LEFT)
+        conn.send(SERVER_TURN_LEFT)
+    elif coordsVisited[1][0] > coordsVisited[1][1] and coordsVisited[1][0] < 0:
+        conn.send(SERVER_TURN_LEFT)
+        conn.send(SERVER_TURN_LEFT)
+    currentCoords = coordsVisited[1]
+    if currentCoords[0] == 0 or currentCoords[1] == 0:
+        while currentCoords != [0,0]:
+            # do I have the correct direction?
+            # handle obsacles
+            conn.send(SERVER_MOVE)
+    else:
+        while currentCoords != [0,0]:
+            print(f"[CURRENT COORDINATES: {currentCoords[0]}:{currentCoords[1]}]")
+            conn.send(SERVER_MOVE)
+            resp = conn.recv(1024)[:-2].decode()
+            if resp.split(" ") == currentCoords:
+                currentCoords = handleObstacle(conn, currentCoords)
+            else:
+                # handle 
+                conn.send(SERVER_MOVE)
+    # retrieve secret
+    conn.send(SERVER_PICK_UP)
+    resp = conn.recv(1024)[:-2].decode()
+    if resp != "RECHARCHING":
+        conn.send(SERVER_LOGOUT)
 
 def handleClient(conn, addr):
     print(f"[CONNECTION FROM {addr}]")
@@ -69,7 +117,11 @@ def handleClient(conn, addr):
         print(f"[GOT MESSAGE: {req[:-2].decode()}]")
         username = req[:-2].decode()
         if len(username) <= 20 and username != "RECHARGING":            
-            auth(conn)
+            usernameHash += (((ord(char) for char in username)) * 1000) % 65536
+            auth(conn, usernameHash)
+            print("[AUTH DONE]")
+        getToCenter(conn)
+            
                 
 
     conn.close()
